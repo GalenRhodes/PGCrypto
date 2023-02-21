2c75f590fe2649775632bd0a60bcb930f6b1fb68
GalenRhodes <galenrhodes@me.com>
2023-02-21 13:53:04 -0500

    Initial Commit

--- src/main/java/com/projectgalen/lib/crypto/Crypto.java
+++ src/main/java/com/projectgalen/lib/crypto/Crypto.java
@@ -1,6 +1,7 @@
 package com.projectgalen.lib.crypto;
 
 import com.projectgalen.lib.utils.PGProperties;
+import com.projectgalen.lib.utils.PGResourceBundle;
 import com.projectgalen.lib.utils.U;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.jetbrains.annotations.NotNull;
@@ -16,28 +17,60 @@ import java.security.*;
 import java.security.spec.X509EncodedKeySpec;
 import java.util.Base64;
 
-@SuppressWarnings("unused")
+@SuppressWarnings({ "unused" })
 public final class Crypto {
 
-    private static final PGProperties props = PGProperties.getXMLProperties("crypto_settings.xml", Crypto.class);
-
-    public static final int    AES_KEY_LENGTH             = props.getInt("crypto.aes.key_length");
-    public static final int    DIFFIE_HELLMAN_KEY_LENGTH  = props.getInt("crypto.diffie_hellman.key_length");
-    public static final int    IV_LENGTH                  = props.getInt("crypto.iv.length");
-    public static final String AES_ALGORITHM              = props.getProperty("crypto.aes.algorithm");
-    public static final String AES_TRANSFORMATION_NO_IV   = props.getProperty("crypto.aes.transformation.no_iv");
-    public static final String AES_TRANSFORMATION_WITH_IV = props.getProperty("crypto.aes.transformation.with_iv");
-    public static final String BOUNCY_CASTLE_PROVIDER     = props.getProperty("crypto.bouncy-castle.provider");
-    public static final String DIFFIE_HELLMAN_ALGORITHM   = props.getProperty("crypto.diffie_hellman.algorithm");
+    private static final PGResourceBundle msgs  = PGResourceBundle.getXMLPGBundle("com.projectgalen.lib.crypto.crypto_messages");
+    private static final PGProperties     props = PGProperties.getXMLProperties("crypto_settings.xml", Crypto.class);
 
     private final IvParameterSpec iv;
     private final SecretKey       secretKey;
+    private final PublicKeyInfo   publicKeyInfo;
+    private final KeyPair         keyPair;
 
     public Crypto(@NotNull DiffieHellmanHandshakeDelegate delegate) throws Exception {
-        KeyPair       keyPair       = generateKeyPair();
-        PublicKeyInfo publicKeyInfo = delegate.getPublicKeyInfo(getBase64EncodedPublicKey(keyPair));
-        secretKey = createSharedSecret(keyPair.getPrivate(), publicKeyInfo.getPublicKey());
-        iv        = new IvParameterSpec(decryptBytes(secretKey, publicKeyInfo.getIv()));
+        keyPair       = generateKeyPair();
+        publicKeyInfo = delegate.getPublicKeyInfo(getBase64EncodedPublicKey(keyPair));
+        secretKey     = createSharedSecret(keyPair.getPrivate(), publicKeyInfo.getPublicKey());
+        iv            = new IvParameterSpec(decryptBytes(secretKey, publicKeyInfo.getIv()));
+    }
+
+    public @Override String toString() {
+        StringBuilder sb = new StringBuilder();
+        String        f1 = props.getProperty("to.str.fmt1");
+        String        f2 = props.getProperty("to.str.fmt2");
+
+        sb.append(String.format(f1, msgs.getString("msg.label.provider"), props.getProperty("crypto.bouncy-castle.provider"))).append("; ");
+        sb.append(String.format(f1, msgs.getString("msg.label.public_key_info"), publicKeyInfo)).append("; ");
+        sb.append(String.format(f2, msgs.getString("msg.label.iv_length"), props.getInt("crypto.iv.length"))).append("; ");
+        sb.append(String.format(f1, msgs.getString("msg.label.aes_algorithm"), props.getProperty("crypto.aes.algorithm"))).append("; ");
+        sb.append(String.format(f2, msgs.getString("msg.label.aes_key_length"), props.getInt("crypto.aes.key_length"))).append("; ");
+        sb.append(String.format(f1, msgs.getString("msg.label.aes_transform"), props.getProperty("crypto.aes.transformation.with_iv"))).append("; ");
+        sb.append(String.format(f1, msgs.getString("msg.label.aes_transform_no_iv"), props.getProperty("crypto.aes.transformation.no_iv"))).append("; ");
+        sb.append(String.format(f1, msgs.getString("msg.label.diffie_hellman_algorithm"), props.getProperty("crypto.diffie_hellman.algorithm"))).append("; ");
+        sb.append(String.format(f2, msgs.getString("msg.label.diffie_hellman_key_length"), props.getInt("crypto.diffie_hellman.key_length"))).append(';');
+
+        return sb.toString();
+    }
+
+    public IvParameterSpec getIv() {
+        return iv;
+    }
+
+    public KeyPair getKeyPair() {
+        return keyPair;
+    }
+
+    public PublicKeyInfo getPublicKeyInfo() {
+        return publicKeyInfo;
+    }
+
+    public SecretKey getSecretKey() {
+        return secretKey;
+    }
+
+    public static @NotNull Provider getProvider() {
+        return Security.getProvider(props.getProperty("crypto.bouncy-castle.provider"));
     }
 
     public @NotNull String decrypt(@NotNull String base64EncodedCipherText) throws GeneralSecurityException {
@@ -61,7 +94,7 @@ public final class Crypto {
     }
 
     public static @NotNull SecretKeySpec createSecretKeyFromDigest(byte @NotNull [] digest) {
-        return new SecretKeySpec(digest, AES_ALGORITHM);
+        return new SecretKeySpec(digest, props.getProperty("crypto.aes.algorithm"));
     }
 
     public static @NotNull SecretKey createSecreteKeyFromDigest(String encDigest) {
@@ -85,7 +118,7 @@ public final class Crypto {
     }
 
     public static byte @NotNull [] createSharedSecretDigest(@NotNull PrivateKey privateKey, byte @NotNull [] publicKeyBytes) throws GeneralSecurityException {
-        KeyFactory keyFactory = KeyFactory.getInstance(DIFFIE_HELLMAN_ALGORITHM, BOUNCY_CASTLE_PROVIDER);
+        KeyFactory keyFactory = KeyFactory.getInstance(props.getProperty("crypto.diffie_hellman.algorithm"), props.getProperty("crypto.bouncy-castle.provider"));
         return createSharedSecretDigest(privateKey, keyFactory.generatePublic(new X509EncodedKeySpec(publicKeyBytes)));
     }
 
@@ -96,7 +129,7 @@ public final class Crypto {
     }
 
     public static byte @NotNull [] decryptBytes(@NotNull SecretKey secretKey, byte @NotNull [] cipherTextData) throws GeneralSecurityException {
-        Cipher c = Cipher.getInstance(AES_TRANSFORMATION_NO_IV);
+        Cipher c = Cipher.getInstance(props.getProperty("crypto.aes.transformation.no_iv"));
         c.init(Cipher.DECRYPT_MODE, secretKey);
         return c.doFinal(cipherTextData);
     }
@@ -110,7 +143,7 @@ public final class Crypto {
     }
 
     public static byte @NotNull [] decryptBytes(@NotNull SecretKey secretKey, @NotNull IvParameterSpec iv, byte @NotNull [] cipherTextData) throws GeneralSecurityException {
-        Cipher c = Cipher.getInstance(AES_TRANSFORMATION_WITH_IV);
+        Cipher c = Cipher.getInstance(props.getProperty("crypto.aes.transformation.with_iv"));
         c.init(Cipher.DECRYPT_MODE, secretKey, iv);
         return c.doFinal(cipherTextData);
     }
@@ -136,7 +169,7 @@ public final class Crypto {
     }
 
     public static @NotNull String encryptData(@NotNull SecretKey secretKey, @NotNull IvParameterSpec iv, byte @NotNull [] plainTextData) throws GeneralSecurityException {
-        Cipher c = Cipher.getInstance(AES_TRANSFORMATION_WITH_IV);
+        Cipher c = Cipher.getInstance(props.getProperty("crypto.aes.transformation.with_iv"));
         c.init(Cipher.ENCRYPT_MODE, secretKey, iv);
         return Base64.getEncoder().encodeToString(c.doFinal(plainTextData));
     }
@@ -146,24 +179,24 @@ public final class Crypto {
     }
 
     public static @NotNull String encryptData(@NotNull SecretKey secretKey, byte @NotNull [] plainTextData) throws GeneralSecurityException {
-        Cipher c = Cipher.getInstance(AES_TRANSFORMATION_NO_IV);
+        Cipher c = Cipher.getInstance(props.getProperty("crypto.aes.transformation.no_iv"));
         c.init(Cipher.ENCRYPT_MODE, secretKey);
         return Base64.getEncoder().encodeToString(c.doFinal(plainTextData));
     }
 
     public static @NotNull IvParameterSpec generateIv() {
-        return new IvParameterSpec(getRandom(IV_LENGTH));
+        return new IvParameterSpec(getRandom(props.getInt("crypto.iv.length")));
     }
 
     public static @NotNull KeyPair generateKeyPair() throws GeneralSecurityException {
-        KeyPairGenerator keyGen = KeyPairGenerator.getInstance(DIFFIE_HELLMAN_ALGORITHM, BOUNCY_CASTLE_PROVIDER);
-        keyGen.initialize(DIFFIE_HELLMAN_KEY_LENGTH);
+        KeyPairGenerator keyGen = KeyPairGenerator.getInstance(props.getProperty("crypto.diffie_hellman.algorithm"), props.getProperty("crypto.bouncy-castle.provider"));
+        keyGen.initialize(props.getInt("crypto.diffie_hellman.key_length"));
         return keyGen.genKeyPair();
     }
 
     public static @NotNull SecretKey generateSecretKey() throws NoSuchAlgorithmException {
-        KeyGenerator keyGenerator = KeyGenerator.getInstance(AES_ALGORITHM);
-        keyGenerator.init(AES_KEY_LENGTH);
+        KeyGenerator keyGenerator = KeyGenerator.getInstance(props.getProperty("crypto.aes.algorithm"));
+        keyGenerator.init(props.getInt("crypto.aes.key_length"));
         return keyGenerator.generateKey();
     }
 
@@ -176,13 +209,9 @@ public final class Crypto {
     }
 
     public static @NotNull KeyAgreement getKeyAgreement(@NotNull PrivateKey privateKey) throws GeneralSecurityException {
-        KeyAgreement keyAgree = KeyAgreement.getInstance(DIFFIE_HELLMAN_ALGORITHM, BOUNCY_CASTLE_PROVIDER);
+        KeyAgreement keyAgree = KeyAgreement.getInstance(props.getProperty("crypto.diffie_hellman.algorithm"), props.getProperty("crypto.bouncy-castle.provider"));
         keyAgree.init(privateKey);
         return keyAgree;
-    }
-
-    public static @NotNull Provider getProvider() {
-        return Security.getProvider(BOUNCY_CASTLE_PROVIDER);
     }
 
     public static byte @NotNull [] getRandom(int size) {
--- src/main/java/com/projectgalen/lib/crypto/PublicKeyInfo.java
+++ src/main/java/com/projectgalen/lib/crypto/PublicKeyInfo.java
@@ -1,11 +1,14 @@
 package com.projectgalen.lib.crypto;
 
+import com.projectgalen.lib.utils.PGResourceBundle;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.Objects;
 
 public class PublicKeyInfo {
+    private static final PGResourceBundle msgs = PGResourceBundle.getXMLPGBundle("com.projectgalen.lib.crypto.crypto_messages");
+
     protected @NotNull String publicKey;
     protected @NotNull String iv;
 
@@ -14,20 +17,24 @@ public class PublicKeyInfo {
         this.iv        = iv;
     }
 
-    public @NotNull String getIv() {
-        return iv;
-    }
-
-    public @NotNull String getPublicKey() {
-        return publicKey;
+    public @Override boolean equals(@Nullable Object o) {
+        return ((this == o) || ((o instanceof PublicKeyInfo) && _equals((PublicKeyInfo)o)));
     }
 
     public @Override int hashCode() {
         return Objects.hash(publicKey, iv);
     }
 
-    public @Override boolean equals(@Nullable Object o) {
-        return ((this == o) || ((o instanceof PublicKeyInfo) && _equals((PublicKeyInfo)o)));
+    public @Override String toString() {
+        return msgs.format("to.str.public_key_info", publicKey, iv);
+    }
+
+    public @NotNull String getIv() {
+        return iv;
+    }
+
+    public @NotNull String getPublicKey() {
+        return publicKey;
     }
 
     private boolean _equals(@NotNull PublicKeyInfo that) {
